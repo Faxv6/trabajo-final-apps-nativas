53e689cafc9fe72ba3380220ae711da0c2231a4b
Facu <facundoracca006@gmail.com>
2025-10-29 10:20:56 -0300

    Creación componente home

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,29 +9,28 @@ import { loginData } from '../interface/auth';
 export class AuthService {
   loggeado: boolean = false;
   router = inject(Router)
-  token:null|string = localStorage.getItem("token");
+  token: null | string = localStorage.getItem("token");
 
-  async login(loginData: loginData){
+  async login(loginData: loginData) {
     this.loggeado = true;
-    const res = await fetch("https://restaurant-api.somee.com/api/Authentication/login",{
+    const res = await fetch("https://restaurant-api.somee.com/api/Authentication/login", {
       method: "POST",
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(loginData)
     })
 
-  if(res.ok){
-    this.token = await res.text()
-    localStorage.setItem("token",this.token)
-    this.router.navigate([""])
-  }
-    console.log("Respuesta del back",res);
+    if (res.ok) {
+      this.token = await res.text()
+      localStorage.setItem("token", this.token)
+      this.router.navigate([""])
+    }
+    console.log("Respuesta del back", res);
   }
 
-
-  logout(){
+  logout() {
     this.token = null
     localStorage.removeItem("token")
     this.router.navigate(["/login"])
   }
-  
+
 }
